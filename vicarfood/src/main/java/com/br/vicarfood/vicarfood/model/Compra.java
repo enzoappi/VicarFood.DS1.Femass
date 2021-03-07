af4273e2f12998210357c757bb3c912213f510d8
KathyGomes <gomes.kathy01@gmail.com>
2021-03-07 14:32:09 -0300

    Mudança Cliente,Criei Compra, Item e Pagamento

---
+++ vicarfood/src/main/java/com/br/vicarfood/vicarfood/model/Compra.java
@@ -0,0 +1,83 @@
+package com.br.vicarfood.vicarfood.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCompra;
+    private Date  dataCompra;
+    private double valorCompra;
+    private int quantidadeProdutos;
+
+    @OneToOne
+    private FormaPagamento formaPagamento;
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+    
+    public Date getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(Date dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public double getValorCompra() {
+        return valorCompra;
+    }
+
+    public void setValorCompra(double valorCompra) {
+        this.valorCompra = valorCompra;
+    }
+
+    public int getQuantidadeProdutos() {
+        return quantidadeProdutos;
+    }
+        
+    public void setQuantidadeProdutos(int quantidadeProdutos) {
+        this.quantidadeProdutos = quantidadeProdutos;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + idCompra;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Compra other = (Compra) obj;
+        if (idCompra != other.idCompra)
+            return false;
+        return true;
+    }
+
+    public FormaPagamento getFormaPagamento() {
+        return formaPagamento;
+    }
+    public void setFormaPagamento(FormaPagamento formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+}
