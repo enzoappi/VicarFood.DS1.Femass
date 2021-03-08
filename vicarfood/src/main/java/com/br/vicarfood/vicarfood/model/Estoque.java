ca793ab4ee7b5d4ee47f4376b616e4e6c76b7997
Enzo T. Appi <enzoappi.tiriba@gmail.com>
2021-03-08 09:37:58 -0300

    Criacao de Estoque, Imagem, Produto, Situacao e TipoProdutoEstoque

--- vicarfood/src/main/java/com/br/vicarfood/vicarfood/model/Estoque.java
+++ vicarfood/src/main/java/com/br/vicarfood/vicarfood/model/Estoque.java
@@ -4,26 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class ItemCompra {
-    
+public class Estoque {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private int quantidadeProduto;
+    private Integer quantidadeEstoque;
 
     @ManyToOne
-    private Compra compra;
-
-    @ManyToOne
-    private Produto produto;
-
-    public void setQuantidadeProduto(int quantidadeProduto) {
-        this.quantidadeProduto = quantidadeProduto;
-    }
+    private TipoProdutoEstoque tipoProdutoEstoque;
 
     public Long getId() {
         return id;
@@ -33,15 +25,24 @@ public class ItemCompra {
         this.id = id;
     }
 
-    public int getQuantidadeProduto() {
-        return quantidadeProduto;
+    public Integer getQuantidadeEstoque() {
+        return quantidadeEstoque;
     }
-    
+
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    @Override
+    public String toString() {
+        return quantidadeEstoque;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + quantidadeProduto;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
 
@@ -53,25 +54,20 @@ public class ItemCompra {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        ItemCompra other = (ItemCompra) obj;
-        if (quantidadeProduto != other.quantidadeProduto)
+        Estoque other = (Estoque) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
             return false;
         return true;
     }
-    
-    public Compra getCompra() {
-        return compra;
+
+    public TipoProdutoEstoque getTipoProdutoEstoque() {
+        return tipoProdutoEstoque;
     }
 
-    public void setCompra(Compra compra) {
-        this.compra = compra;
-    }
-
-    public Produto getProduto() {
-        return produto;
-    }
-
-    public void setProduto(Produto produto) {
-        this.produto = produto;
+    public void setTipoProdutoEstoque(TipoProdutoEstoque tipoProdutoEstoque) {
+        this.tipoProdutoEstoque = tipoProdutoEstoque;
     }    
 }
