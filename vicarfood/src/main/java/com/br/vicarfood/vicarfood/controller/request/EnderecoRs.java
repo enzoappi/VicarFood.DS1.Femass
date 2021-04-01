397672c5aa4b9bb0d4018b98a62005cc65d7a713
Enzo T. Appi <enzoappi.tiriba@gmail.com>
2021-04-01 20:24:28 -0300

    Criacao das Classes EnderecoController, EnderecoRs.

---
+++ vicarfood/src/main/java/com/br/vicarfood/vicarfood/controller/request/EnderecoRs.java
@@ -0,0 +1,49 @@
+package com.br.vicarfood.vicarfood.controller.request;
+
+public class EnderecoRs {
+    private Long id;
+    private String logradouro;
+    private String numero;
+    private String nome;
+    private Double valor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }        
+}
