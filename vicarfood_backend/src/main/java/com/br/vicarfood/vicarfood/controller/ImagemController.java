b52ad562dd00ba8a422ecd330fe7d9772012520c
Enzo T. Appi <enzoappi.tiriba@gmail.com>
2021-05-03 16:42:24 -0300

    Criacao da tela de edicao e request de edicao e gravacao

--- vicarfood_backend/src/main/java/com/br/vicarfood/vicarfood/controller/ImagemController.java
+++ vicarfood_backend/src/main/java/com/br/vicarfood/vicarfood/controller/ImagemController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/imagem")
 public class ImagemController {
