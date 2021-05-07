8bd9bc515058f7f30ff70e073da492c66bc453c3
Diego Torres <diegosanttorres@hotmail.com>
2021-05-07 07:52:49 -0300

    Pull Origin

--- vicarfood/frontend/src/pedido/Pedido.js
+++ vicarfood/frontend/src/pedido/Pedido.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Pedido extends Component {
     render() {
         return (
-            <div className="mt-5 pt-4">
+            <div className="mt-5 pt-5">
                 <h3>Informações dos pedidos</h3>
             </div>
         )
