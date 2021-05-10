ba7b1697f39856b2f70763a362cc76d93e66857c
Diego Torres <diegosanttorres@hotmail.com>
2021-05-10 10:36:08 -0300

    alteracoes branch

---
+++ vicarfood_frontend/src/pedido/Pedido.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Pedido extends Component {
+    render() {
+        return (
+            <div className="mt-5 pt-5">
+                <h3>Informações dos pedidos</h3>
+            </div>
+        )
+    }
+}
