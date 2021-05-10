a8a42c5165642c954d2f453b69b348cf403ea9a6
Diego Torres <diegosanttorres@hotmail.com>
2021-05-10 10:30:08 -0300

    alteracoes branch

---
+++ vicarfood_backend/src/main/java/com/br/vicarfood/vicarfood/controller/EnderecoController.java
@@ -0,0 +1,107 @@
+package com.br.vicarfood.vicarfood.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.br.vicarfood.vicarfood.controller.request.EnderecoRequest;
+import com.br.vicarfood.vicarfood.model.Bairro;
+import com.br.vicarfood.vicarfood.model.Endereco;
+import com.br.vicarfood.vicarfood.repository.BairroRepository;
+import com.br.vicarfood.vicarfood.repository.EnderecoRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/endereco")
+public class EnderecoController {
+    private final EnderecoRepository enderecoRepository;
+    private final BairroRepository bairroRepository;
+
+    public EnderecoController(EnderecoRepository enderecoRepository, BairroRepository bairroRepository) {
+        this.enderecoRepository = enderecoRepository;
+        this.bairroRepository = bairroRepository;
+    }
+    
+    @CrossOrigin
+    @GetMapping("/listar")
+    public List<EnderecoRequest> getEnderecos() {
+        List<Endereco> enderecos = enderecoRepository.findAll();
+
+        List<EnderecoRequest> endrs = new ArrayList<EnderecoRequest>();
+        for(Endereco endereco : enderecos) {
+            EnderecoRequest e = new EnderecoRequest();
+            e.setId(endereco.getId());
+            e.setLogradouro(endereco.getLogradouro());
+            e.setNumero(endereco.getNumero());
+            e.setNomeBairro(endereco.getBairro().getNomeBairro());
+            endrs.add(e);
+        }
+
+        return endrs;
+    }
+
+    @CrossOrigin
+    @PostMapping("/incluir")
+    public void incluir(@RequestBody EnderecoRequest enderecoRequest) throws Exception{
+        Endereco endereco = new Endereco();
+        endereco.setLogradouro(enderecoRequest.getLogradouro());
+        endereco.setNumero(enderecoRequest.getNumero());
+
+        List <Bairro> bairros = bairroRepository.findAll();
+
+        Bairro bairro = null;
+        for(Bairro b : bairros){
+            if(b.getNomeBairro().equals(enderecoRequest.getNomeBairro())){
+                bairro = b;
+            }
+        }
+
+        if(bairro == null){
+            throw new Exception("Bairro não cadastrado");            
+        }
+        endereco.setBairro(bairro);
+
+        enderecoRepository.save(endereco);
+    }
+
+    @CrossOrigin
+    @PostMapping("/alterar")
+    public void alterarEndereco(@RequestBody EnderecoRequest enderecoRequest) throws Exception {
+        var objeto = enderecoRepository.findById(enderecoRequest.getId());
+        
+        if(objeto.isPresent()) {
+            Endereco endereco = objeto.get();
+            endereco.setLogradouro(enderecoRequest.getLogradouro());
+            endereco.setNumero(enderecoRequest.getNumero());
+            enderecoRepository.save(endereco);
+        } else{
+            throw new Exception("Endereço não encontrado!");
+        }       
+
+    }
+
+    @CrossOrigin
+    @PostMapping("/excluir/{id}")
+    public void excluirEndereco(@PathVariable("id") Long id) throws Exception{
+        try {
+            enderecoRepository.deleteById(id);
+        } catch(Exception e) {
+            throw new Exception("Endereco nao encontrado!");
+        }
+        
+        /*var e = enderecoRepository.findById(id);
+
+        if(e.isPresent()) {
+            Endereco endereco = e.get();
+            enderecoRepository.delete(endereco);
+        } else {
+            throw new Exception("Endereco nao encontrado");
+        }*/
+    }
+}
