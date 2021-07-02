d79f1e8f122aaa63adefba2149dc7954c5c09687
Kathleen Gomes <kathleen.gomes@sapiensia.com>
2021-07-02 00:08:51 -0300

    imagens main

--- vicarfood_frontend/src/Components/main/Main.js
+++ vicarfood_frontend/src/Components/main/Main.js
@@ -2,9 +2,9 @@ import React from 'react'
 import '../main/Main.css'
 
 import Container from '../Container'
-import batata_rosti_jpeg from "../../assets/images/batata_rosti.jpeg"
-import costela_jpg from "../../assets/images/costela.jpg"
-import costela_bovina_png from "../../assets/images/costela_bovina 3.jpg"
+import batata_rosti_jpeg from "../../assets/images/teste2.jpg"
+import costela_jpg from "../../assets/images/costela2.jpg"
+import costela_bovina_png from "../../assets/images/costela_bovina4.jpg"
 
 const Main = () => (
     
@@ -12,30 +12,30 @@ const Main = () => (
         <Container>
         <h1>Main</h1>
         <div className="carrosel">
-                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-indicators">
+                <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-indicators">
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                         </div>
-                        <div class="carousel-inner ">
-                                <div class="carousel-item active">
+                        <div className="carousel-inner ">
+                                <div className="carousel-item active">
                                         <img src={batata_rosti_jpeg} class="d-block w-100 " alt="..." />
                                 </div>
-                                <div class="carousel-item">
+                                <div className="carousel-item">
                                         <img src={costela_jpg} class="d-block w-100" alt="..." />
                                 </div>
-                                <div class="carousel-item">
+                                <div className="carousel-item">
                                         <img src={costela_bovina_png} class="d-block w-100" alt="..." />
                                 </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
                         </button>
                 </div>
         </div>
